df36d51794c592ef7e5b40cb78b4e079f60ba192
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2022-03-24 21:09:57 +0800

    Created main.py and module 'settings.py'

--- src/main.py
+++ src/main.py
@@ -1,8 +1,34 @@
+"""
+Masterlist
+
+A List Generator-typed Android application designed to be Quality-of-life focused.
+
+GitHub URL: https://github.com/Thanatisia/masterlist-app
+"""
+
+# Import Internal Libraries
 import os
 import sys
+import kivy
+
+# Import External Libraries
+import modules.settings as settings
+from modules.settings import app, database
+
+
+def init():
+	"""
+	Class Initialization
+	"""
+	PROJ_INFO = app.init("Masterlist", "v20220324_1", "https://github.com/Thanatisia/masterlist-app")
+	DB_INFO = database.init("MyDB", "1", "root", "")
+
+	print(PROJ_INFO)
+	print(DB_INFO)
 
 def main():
-	print("Hello World")
+	print("Hello World")		
 
 if __name__ == "__main__":
+	init()
 	main()
